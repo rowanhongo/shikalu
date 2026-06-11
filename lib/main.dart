0768e4603f79efc9b17996cd1434d857463543e8
rowanhongo <r.hongo@alustudent.com>
2026-06-11 21:57:11 +0200

    feat:added provider dependancy to pubspec.yaml and added provider to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:provider/provider.dart';
+import 'services/auth_service.dart';
+import 'screens/welcomepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AuthService(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ShikALU',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF6C63FF)),
+        useMaterial3: true,
+      ),
+      home: const WelcomePage(),
+    );
+  }
 }
 
